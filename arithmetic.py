b057068f6bd0064e9aa89ff0dbec417726881275
Stefan Brandle <sbrandle@gmail.com>
2024-09-20 15:51:03 +0000

    Added min_rand()

--- arithmetic.py
+++ arithmetic.py
@@ -5,3 +5,11 @@ def sub(a,b):
   Bill Nicholson
   """
   return a - b
+
+def min_rand():
+  """
+  Returns smallest random number
+  """
+  return 17
+
+print("The smallest random number is", min_rand())
